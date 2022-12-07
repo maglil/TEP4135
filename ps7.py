8a9b0831ffedcd90de1b5fb97d13a14bfc19091b
Magnus Lilledahl <29771060+NLOM-NTNU-PI@users.noreply.github.com>
2022-12-07 16:17:57 +0100

    ps7

--- ps7.py
+++ ps7.py
@@ -1,5 +1,6 @@
 # Problem set 7 - TEP4135
 
+from math import sqrt
 from scipy.optimize import root_scalar
 
 def Mfun(M,k):
@@ -11,14 +12,46 @@ def p_ratio(M,k,ratio=0):
 def critical_area_ratio(M,k,ratio=0):
     return 1/M * ( (1+0.5*(k-1)*M**2) / (0.5*(k+1)) )**(0.5*(k+1)/(k-1))-ratio
 
+def a_ratio(M,k):
+    return Mfun(M,k)**(1/2)
+
+def rho_ratio(M,k):
+    return Mfun(M,k)**(1/(k-1))
+
+
+
 # Problem 1
 print("Problem 1")
 
+T0 = 273+20
+p0 = 100e5
+R = 287
 A = 1e-3
+A1 = 1e-3
 A_star = 5e-4
-k = 1.5
+A2 = 5e-4
+k = 1.4
 
 # Problem 1a
+print("-1a-")
+
+a0 = sqrt(k*R*T0)
+print(f"Stagnation speed of sound is {a0:.4}")
+a1 = a0/a_ratio(1,k)
+print(f"Critical speed of sound is {a1:.4}")
+rho0 = p0/(R*T0)
+print(f'STagnation density is {rho0:.4}')
+rho1 = rho0/rho_ratio(1,k)
+print(f'Density is {rho1:.4}')
+mdot = rho1*a1*A1
+
+rho2 = mdot/(a1*A2)
+
+rho02 = rho2*rho_ratio(1,k)
+
+p1 = rho02*R*T0
+print(f'Pressure in tank is {p1:.4}')
+
 p01 = 20
 
 # Problem 1b
